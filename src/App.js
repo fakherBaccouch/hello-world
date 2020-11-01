1c36064985771ff63423b21e5f45d838e270090e
FakherBaccouch <bakouchfakher@gmail.com>
2020-11-01 20:51:51 +0100

    11/1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home"
 
 function App() {
   return (
     <div className="App">
-      welcome to my burger app
+       <Home/>
     </div>
   );
 }
