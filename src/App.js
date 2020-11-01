6507693ecb48873cbd63a7e7402b965ebd63d444
FakherBaccouch <bakouchfakher@gmail.com>
2020-11-01 23:11:27 +0100

    navbar creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import Home from "./components/home"
+import Navbar from "./components/navbar"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './styling/AppScss.scss';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-       <Home/>
-    </div>
+   
+<div className="App">
+  <Router>
+      <Navbar/>
+      <Switch>
+          <Route exact path="/">
+           welcome to home page
+          </Route>
+          <Route path="/addorder">
+           Add order
+          </Route>
+          <Route path="/myorders">
+            lost of orders
+          </Route>
+        </Switch>
+  </Router>
+ 
+</div>
+    
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,30 @@
+import React from "react"
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+const  Navbar=()=>{
+    return (
+        <div className="navbar" >
+          <h1 className="brand">FAKHER</h1>
+               <ul>
+          <li>
+         <a> <Link to="/addorder">ADD ORDER</Link></a>  
+          </li>
+          <li>
+         <a><Link to="/myorders">ORDERS</Link></a>   
+          </li>
+          <li>
+           <a><Link to="/checkout">CHECKOUT</Link></a> 
+          </li>
+        </ul>
+        <ul > <div><PermIdentityIcon style={{color:"white",fontSize:"25px",marginLeft:"20px"}}/>lol</div> <div><ShoppingCartRoundedIcon style={{color:"white",fontSize:"25px",marginLeft:"20px"}}/></div></ul>
+       
+        </div>
+    )
+}
+export default Navbar
